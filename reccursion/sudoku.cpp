fe95726ca6bb597f9d5e1eb10c4fdb09d3ffaee9
Aanandgupta <anand.1923cs1140@kiet.edu>
2022-02-07 20:00:00 -0600

    check

--- reccursion/sudoku.cpp
+++ reccursion/sudoku.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-
-
-
 int main()
 {
-    vector <vector<int>> ary={
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {},
-        {}
-    };
-     
+    int all,state,month,temp;
+    vector<int> store;
+
+    cin>>all>>state>>month;
+    for(int i=0;i<all;i++)
+    {
+        cin>>temp;
+        store.push_back(temp);
+    }
+
+    int country=(month-1)/2;
     return 0;
 }
